2f5a569972935f5c4faf7df3be60ca3b7a303e69
gauxs <sgau0522@gmail.com>
2026-09-11 15:58:42 +0530

    Connect Four - IV

--- connect_four/connectfour.go
+++ connect_four/connectfour.go
@@ -12,12 +12,17 @@ func Execute() {
 	game := NewGame(6, 7)
 	if err := game.AddPlayer("Player-A", enum.DISK_RED); err != nil {
 		fmt.Println(err.Error())
+		return
 	}
 	if err := game.AddPlayer("Player-B", enum.DISK_BLUE); err != nil {
 		fmt.Println(err.Error())
+		return
 	}
 
-	game.StartGame()
+	if err := game.StartGame(); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
 	for game.GetGameState() == enum.GAMESTATE_PLAYING {
 		col := 0
